1ecb3eb03667f2d6ed106c716306059beaf048ad
Juan Ceballos <jacu29@gmail.com>
2022-01-17 08:40:28 -0500

    Agregar Imagenes

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -1,14 +1,28 @@
-import React from 'react'
-import "../css/Trailer.css"
-import {IoPlay} from "react-icons/io5"
+import React from "react";
+import "../css/Trailer.css";
+import { IoPlay } from "react-icons/io5";
+
+import trailerBackground from "../images/trailer-background.png";
 
 const Trailer = () => {
-    return <div className="Trailer">
-        <button className='play'>
-            <IoPlay />
-        </button>
-        <h1 className='trailer-title'>VER TRAILER</h1>
-    </div>;
-}
+    return (
+        <div className="Trailer">
+            <div className="trailer-container container">
+                <img
+                    className="trailer-image"
+                    src={trailerBackground}
+                    alt="Trailer Background"
+                />
+                <div className="trailer-filter"></div>
+                <div className="trailer-content">
+                    <button className="play">
+                        <IoPlay />
+                    </button>
+                    <h1 className="trailer-title">VER TRAILER</h1>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default Trailer
+export default Trailer;
